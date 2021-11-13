111241fbe2efb996bc7edecefdd4a9706f9831d2
Devrats <devvratsharma9759@gmail.com>
2021-11-13 19:09:36 +0530

    Initial commit

---
+++ src/main/java/com/example/gurukul/GurukulApplication.java
@@ -0,0 +1,13 @@
+package com.example.gurukul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurukulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GurukulApplication.class, args);
+	}
+
+}
