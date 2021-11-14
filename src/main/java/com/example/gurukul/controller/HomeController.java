a70be61179db0647974edea1edcda9a9e61fd969
Devrats <devvratsharma9759@gmail.com>
2021-11-14 13:37:16 +0530

    created testing method

--- src/main/java/com/example/gurukul/controller/HomeController.java
+++ src/main/java/com/example/gurukul/controller/HomeController.java
@@ -8,10 +8,30 @@
 package com.example.gurukul.controller;
 
 
+import com.example.gurukul.entity.Student;
+import com.example.gurukul.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RestController
 public class HomeController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @ResponseBody
+    @RequestMapping(value = "/enter", method = RequestMethod.POST)
+    public ResponseEntity<?> login(@RequestBody HashMap<String, Object> map) {
+        Student student = new Student();
+        student.setId(Long.parseLong((String) map.get("id")));
+        studentRepository.save(student);
+        return ResponseEntity.ok(Map.of("idea",1234565));
+    }
+
 }
