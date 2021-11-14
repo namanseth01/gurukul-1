8be2279dfc183e00fce35c46c03f0743af6b6b88
andyrude <anirudh.mishra_cs19@gla.ac.in>
2021-11-14 14:39:56 +0530

    commit

--- src/main/java/com/example/gurukul/controller/UserController.java
+++ src/main/java/com/example/gurukul/controller/UserController.java
@@ -7,10 +7,37 @@
 
 package com.example.gurukul.controller;
 
+import com.example.gurukul.entity.Classes;
+import com.example.gurukul.entity.Student;
+import com.example.gurukul.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RestController
 public class UserController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @ResponseBody
+    @RequestMapping(value = "/class", method = RequestMethod.POST)
+    public ResponseEntity<?> getClasses(@RequestBody HashMap<String, Object> map){
+        Long id = Long.parseLong((String) map.get("id"));
+        Student student = this.studentRepository.getById(id);
+        List<Classes> classesList = student.getClasses();
+        ArrayList<String> list = new ArrayList<>();
+        for (Classes classes: classesList) {
+            list.add(classes.stringTo());
+        }
+        return ResponseEntity.ok(Map.of("list",list));
+
+    }
 }
