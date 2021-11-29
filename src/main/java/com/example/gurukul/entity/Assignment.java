8f91963c4c24a8b37ecf61a5415961a9222b7d4b
Devrats <devvratsharma9759@gmail.com>
2021-11-30 00:09:41 +0530

    testing

--- src/main/java/com/example/gurukul/entity/Assignment.java
+++ src/main/java/com/example/gurukul/entity/Assignment.java
@@ -9,9 +9,7 @@ package com.example.gurukul.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -19,6 +17,7 @@ import java.util.Objects;
 public class Assignment {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @ManyToOne
     @JsonManagedReference
